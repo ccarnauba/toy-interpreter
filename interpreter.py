0938f3a63bdb97c611f4821500a9f136e6b71a8f
ccarnauba <ccarnauba@hotmail.com>
2026-05-27 12:09:45 -0400

    First version. Added tests, and things are broken :'(.

--- interpreter.py
+++ interpreter.py
@@ -7,24 +7,31 @@ def parse (program : str):
     paren, we then return the list.
 
     >>> parse ("(begin (define r 10) (* pi (* r r)))")
-    ['define', 'r']
+    []
+    [[[]]]
 
     """
-    current_word = ""
-    current_tree = []
-    for letter_index in range(len(program)):
-        current_letter = program[letter_index]
-       # TODO Note that this doesn't actually catch any syntax mistakes, it
-       # only works on the happy case. We need more than this.
-        if program [letter_index]== '(':
-            return current_tree.append([parse(program[letter_index + 1:])])
-        elif program [letter_index] == ')':
-            print (current_tree);
-            return current_tree
-        elif program [letter_index] == ' ':
-            current_tree.append(current_word)
-            current_word = ""
-        else:
-            current_word = current_word + current_letter
+    def rec_parse (current_tree, program):
 
-    return current_tree
+        current_word = ""
+        for letter_index in range(len(program)):
+            current_letter = program[letter_index]
+            # TODO Note that this doesn't actually catch any syntax mistakes, it
+            # only works on the happy case. We need more than this.
+            # Note that it also doesn't really work lol.
+            if program [letter_index]== '(':
+                # This will add an extra list layer that we don't want
+                print (current_tree);
+                current_tree.append([parse(program[letter_index + 1:])])
+            elif program [letter_index] == ')':
+                current_tree.append(current_word)
+                print (current_tree);
+                return current_tree
+            elif program [letter_index] == ' ':
+                current_tree.append(current_word)
+                current_word = ""
+            else:
+                current_word = current_word + current_letter
+                return current_tree
+
+    return rec_parse([], program)
