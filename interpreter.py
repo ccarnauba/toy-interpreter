d123054138e71dfef28cf837941695a9955cdfa2
ccarnauba <ccarnauba@hotmail.com>
2026-05-27 14:07:52 -0400

    Working implementation of tokenizer and paresr

--- interpreter.py
+++ interpreter.py
@@ -1,3 +1,25 @@
+def tokenize (program : str) -> list[str]:
+    """
+    Tokenizes our program on spaces.
+
+    >>> tokenize ("(begin (define r 10) (* pi (* r r)))")
+    ['(', 'begin', '(', 'define', 'r', '10', ')', '(', '*', 'pi', '(', '*', 'r', 'r', ')', ')', ')']
+    >>> tokenize ("(define r 10)")
+    ['(', 'define', 'r', '10', ')']
+    >>> tokenize ("(begin (define r 10))")
+    ['(', 'begin', '(', 'define', 'r', '10', ')', ')']
+    """
+    sanitized_program = ""
+    for char in program:
+        if char == '(':
+            sanitized_program = sanitized_program + (' ( ')
+        elif char == ')':
+            sanitized_program = sanitized_program + (' ) ')
+        else:
+            sanitized_program = sanitized_program + char
+
+    return [ char for char in sanitized_program.split(' ') if char ]
+
 def parse (program : str):
     """
     Recursively creates the ast for our program.
@@ -7,31 +29,32 @@ def parse (program : str):
     paren, we then return the list.
 
     >>> parse ("(begin (define r 10) (* pi (* r r)))")
-    []
-    [[[]]]
-
+    ['begin', ['define', 'r', '10'], ['*', 'pi', ['*', 'r', 'r']]]
+    >>> parse ("(begin (define r 10))")
+    ['begin', ['define', 'r', '10']]
+    >>> parse ("(define r 10)")
+    ['define', 'r', '10']
     """
-    def rec_parse (current_tree, program):
+    def rec_parse(local_tree, tokenized_program : list[str]):
+        if tokenized_program == []:
+            return local_tree
 
-        current_word = ""
-        for letter_index in range(len(program)):
-            current_letter = program[letter_index]
-            # TODO Note that this doesn't actually catch any syntax mistakes, it
-            # only works on the happy case. We need more than this.
-            # Note that it also doesn't really work lol.
-            if program [letter_index]== '(':
-                # This will add an extra list layer that we don't want
-                print (current_tree);
-                current_tree.append([parse(program[letter_index + 1:])])
-            elif program [letter_index] == ')':
-                current_tree.append(current_word)
-                print (current_tree);
-                return current_tree
-            elif program [letter_index] == ' ':
-                current_tree.append(current_word)
-                current_word = ""
+        token = tokenized_program.pop(0)
+
+        while token:
+            if token == '(':
+                program_after_paren = rec_parse([], tokenized_program)
+                local_tree.append(program_after_paren)
+            elif token == ')':
+                return local_tree
             else:
-                current_word = current_word + current_letter
-                return current_tree
+                local_tree.append(token)
 
-    return rec_parse([], program)
+            if tokenized_program:
+                token = tokenized_program.pop(0)
+            else:
+                token = None
+
+        return local_tree
+
+    return rec_parse([], tokenize(program))[0]
